dd28217e64fa82c720f147844c8d9b4fd524bcf1
Jesús Díaz <jesusdz@gmail.com>
2021-01-31 18:56:07 +0100

    All shaders in a single file. Stb image added.

--- Engine/Code/engine.h
+++ Engine/Code/engine.h
@@ -11,6 +11,19 @@ typedef glm::ivec2 ivec2;
 typedef glm::ivec3 ivec3;
 typedef glm::ivec4 ivec4;
 
+struct Image
+{
+    void* pixels;
+    ivec2 size;
+    i32   nchannels;
+    i32   stride;
+};
+
+struct Texture
+{
+    GLuint handle;
+};
+
 struct App
 {
     // Loop
